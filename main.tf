683548d1a19969b80244f720ee30c0e713c8a495
pradeepm2406 <pradeep.mallavarapu@gmail.com>
2021-02-07 12:58:02 +0100

    new files

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    datadog = {
+      source = "Datadog/datadog"
+    }
+  }
+  required_version = ">= 0.11"
+}
+# Configure the Datadog provider
+provider "datadog" {
+  api_key = "7df728769635dcb55cb7c0026c682f56"
+  app_key = "2c0f4261f22532128efc3ee9e30e4e7ea77e3fe7"
+}
+# Create a new monitor
+resource "datadog_monitor" "cpu_anomalous" {
+  name = "Anomalous CPU usage"
+  type = "query alert"
+  message = "CPU utilization is outside normal bounds"
+  query = "avg(last_4h):anomalies(ewma_20(avg:system.cpu.system{env:prod,service:website}.as_rate()), 'robust', 3, direction='below', alert_window='last_30m', interval=60, count_default_zero='true', seasonality='weekly') >= 1"
+  thresholds {
+    critical          = 1.0
+    critical_recovery = 0.0
+  }
+  threshold_windows {
+    trigger_window    = "last_30m"
+    recovery_window   = "last_30m"
+  }
+
+  notify_no_data    = false
+  renotify_interval = 60
+}
